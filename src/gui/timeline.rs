05473963ae84bac61c57923dd912b7b574e108d4
Jeff Tickle <jeff@jefftickle.com>
2025-09-23 09:04:36 -0400

    Working in f32 for anything other than the sample to pixel was a mistake

--- src/gui/timeline.rs
+++ src/gui/timeline.rs
@@ -1,6 +1,6 @@
 use std::{ops::Range, sync::Arc};
 use cpal::SampleRate;
-use egui::{load::SizedTexture, Color32, ColorImage, DragValue, Image, Pos2, TextureOptions, Vec2};
+use egui::{load::SizedTexture, Color32, ColorImage, DragValue, Image, TextureOptions};
 use log::debug;
 use parking_lot::RwLock;
 
@@ -46,8 +46,8 @@ impl Timeline {
         self.screen_to_sample(n)..self.screen_to_sample(n+1).clamp(0, clamp)
     }
 
-    fn screen_to_image_idx(&self, width: f32, x: f32, y: f32) -> usize {
-        (y * width + x) as usize
+    fn screen_to_image_idx(&self, width: usize, x: usize, y: usize) -> usize {
+        ((y * width) + x) as usize
     }
 
     pub fn show(&mut self, ui: &mut egui::Ui) {
@@ -62,8 +62,8 @@ impl Timeline {
         });
 
         // Get the current screen real estate that we have to work with
-        let width = ui.available_size().x;
-        let height = self.height as f32;
+        let width = ui.available_size().x.floor() as usize;
+        let height = self.height;
 
         // I am assuming that egui will scale this properly but it may need to be revisited after
         // experimentation. Look into ui.pixels_per_point() if necessary.
@@ -73,7 +73,7 @@ impl Timeline {
         // Zero is in the center. Lines drawn at +-128
         let mut amplitude_image = std::vec::from_elem(
             Color32::from_gray(0),
-            (width * height) as usize
+            width * height
         );
 
         // Acquire read lock on amplitudes
@@ -106,22 +106,22 @@ impl Timeline {
                 |acc, x| (acc.0.max(*x), acc.1.min(*x))
             );
 
-            let halfheight = height/2f32;
+            let halfheight = (height/2) as f32;
 
-            let displaymax = f32max * halfheight + halfheight;
-            let displaymin = f32min * halfheight + halfheight;
-            amplitude_image[self.screen_to_image_idx(width, i as f32, displaymax)] = Color32::from_rgb(0, 255, 0);
-            amplitude_image[self.screen_to_image_idx(width, i as f32, displaymin)] = Color32::from_rgb(255, 0, 0);
+            let displaymax = (f32max * halfheight + halfheight).floor() as usize;
+            let displaymin = (f32min * halfheight + halfheight).floor() as usize;
+            amplitude_image[self.screen_to_image_idx(width, i, displaymax)] = Color32::from_rgb(0, 255, 0);
+            amplitude_image[self.screen_to_image_idx(width, i, displaymin)] = Color32::from_rgb(255, 0, 0);
         }
 
         // Draw a vertical line for the current pointer position, if any
         if let Some(pointer_pos) = ui.input(|i| i.pointer.latest_pos()) {
             let mut bounds = ui.cursor();
-            bounds.max.y = bounds.min.y + height;
+            bounds.max.y = bounds.min.y + height as f32;
             bounds.max.x = bounds.max.x - 1f32;
             if bounds.contains(pointer_pos) {
                 for i in 0..(height as usize) {
-                    let idx = self.screen_to_image_idx(width, pointer_pos.x - bounds.min.x, i as f32);
+                    let idx = self.screen_to_image_idx(width, (pointer_pos.x - bounds.min.x).floor() as usize, i);
                     amplitude_image[idx] = Color32::from_rgb(0, 0, 255);
                 }
             }
